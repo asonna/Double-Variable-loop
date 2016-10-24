333123b7a0d08fd1746835460a55874f0bfea21a
Annie Sonna and Getro Naissance <annie@>
2016-10-24 13:33:00 -0700

    updated mad-libs with looping and arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,25 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event){
-    var inputName1 = $("input#name1").val();
-    var inputName2 = $("input#name2").val();
-    var inputStreet = $("input#street").val();
-    var inputCity = $("input#city").val();
-    var inputZip = $("input#zip").val();
-
-    $(".name1").text(inputName1);
-    $(".name2").text(inputName2);
-    $(".street").text(inputStreet);
-    $(".city").text(inputCity);
-    $(".zip").text(inputZip);
-
-    $(".letter").show();
+    var inputs = ['name1', 'name2', 'street', 'city', 'zip'];
+    inputs.forEach(function(input) {
+      var userInput = $("input#" + input).val();
+      $("." + input).text(userInput);
+    });
+      $(".letter").show();
 
     event.preventDefault();
   });
 });
+
+//
+// var inputName1 = $("input#name1").val();
+// var inputName2 = $("input#name2").val();
+// var inputStreet = $("input#street").val();
+// var inputCity = $("input#city").val();
+// var inputZip = $("input#zip").val();
+//
+// $(".name1").text(inputName1);
+// $(".name2").text(inputName2);
+// $(".street").text(inputStreet);
+// $(".city").text(inputCity);
+// $(".zip").text(inputZip);
